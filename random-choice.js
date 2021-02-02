90e8b583709557705b389bb70561fffbdaf80ff8
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-02-02 22:26:57 +0100

    feat/random-choice

--- random-choice.js
+++ random-choice.js
@@ -1,14 +1,36 @@
 /**
+ * This file randomly chooses an id from the local database in a json file.
+ * To execute : 
+ * node random-choice.js
+ * 
+ * Authors : Corentin Forler, Pierre Sibut-Bourde, 2021.
+ */
+
+/**
  * Practical requirements.
  */
+const fs = require('fs')
+
 const jsonInput = require('./ids.json');
+const jsonOutput = require('./used-ids.json');
 
 /**
  * Functions that returns a random element from the ids.
  */
 function randomChoice() {
     const index = Math.floor(Math.random()*jsonInput.length);
-    return jsonInput[index];
+    const randomId = jsonInput[index];
+
+    
+    if(jsonOutput.includes(randomId)) {
+        console.log('Already used.');
+        randomChoice()
+    }
+    else {
+        jsonOutput.push(randomId);
+        fs.writeFileSync('./used-ids.json', JSON.stringify(jsonOutput));
+        return randomId;
+    }
 }
 
-console.log(randomChoice());
+randomChoice();
