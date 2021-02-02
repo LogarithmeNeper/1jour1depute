b336d9c3cf869df2f239a351c2200c0e17ca5a5d
Corentin Forler <cogk42@users.noreply.github.com>
2021-02-02 22:58:20 +0100

    updated random choice algorithm + eslint, style, doc

--- random-choice.js
+++ random-choice.js
@@ -1,42 +1,54 @@
 /**
  * This file randomly chooses an id from the local database in a json file, and uses it
  * if it has not been seen before.
- * To execute : 
+ * To execute :
  * node random-choice.js
- * 
+ *
  * Authors : Corentin Forler, Pierre Sibut-Bourde, 2021.
  */
 
 /**
  * Practical requirements.
  */
-const fs = require('fs')
+const fs = require('fs');
 
-const jsonInput = require('./ids.json');
-const jsonOutput = require('./used-ids.json');
+const allIds = require('./ids.json');
+const usedIds = require('./used-ids.json');
 
 /**
  * Checks if there is still one id left.
- * Can be done lazily thanks to the check in randomChoice().
  */
 function isThereAnIdLeft() {
-    return !(jsonInput.length==jsonOutput.length);
+  return allIds.length !== usedIds.length;
+}
+
+/**
+ * @param {Array} array
+ * @returns {any}
+ */
+function pickRandom(array) {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
 }
 
 /**
  * Functions that returns a random element from the ids.
+ * @returns {number | undefined}
  */
 function randomChoice() {
-    const index = Math.floor(Math.random()*jsonInput.length);
-    const randomId = jsonInput[index];
+  const nonUsedIds = allIds.filter(x => !usedIds.includes(x));
+  if (nonUsedIds.length === 0) {
+    return;
+  }
 
-    
-    if(jsonOutput.includes(randomId)) {
-        randomChoice();
-    }
-    else {
-        jsonOutput.push(randomId);
-        fs.writeFileSync('./used-ids.json', JSON.stringify(jsonOutput));
-        return randomId;
-    }
+  // Pick a random non-used id
+  const randomId = pickRandom(nonUsedIds);
+
+  // Update the list of used ids
+  usedIds.push(randomId);
+  fs.writeFileSync('./used-ids.json', JSON.stringify(usedIds));
+
+  return randomId;
 }
+
+module.exports = { randomChoice };
