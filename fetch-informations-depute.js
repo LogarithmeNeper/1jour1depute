511e2f5c8b4b7577dd1e9f067febb1b15cddafdd
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-02-03 11:43:28 +0100

    wip/getDocumentInformation

--- fetch-informations-depute.js
+++ fetch-informations-depute.js
@@ -6,6 +6,20 @@
  * Authors : Corentin Forler, Pierre Sibut-Bourde, 2021.
  */
 
+/**
+ * Practical requirements.
+ */
+const { fetchHtml, parseHtml, flatten } = require('./utils.js');
+
+const { promisify } = require('util');
+const fs = require('fs');
+const writeFile = promisify(fs.writeFile);
+
+
+function getDocument(id) {
+    const url = `https://www2.assemblee-nationale.fr/sycomore/fiche/(num_dept)/${id}`;
+    return fetchHtml(url).then(parseHtml);
+}
 
 /**
  * Functions that extracts the URL of the principal image.
@@ -15,5 +29,5 @@ function getImageUrl(document) {
    const regex = /(.*)sycomore(.*)$/;
    return [...document.querySelectorAll('img')]
    .map(elem => elem.src.match(regex)?.[0])
-   .filter(x => x!=null)
+   .filter(x => x!=null);
 }
