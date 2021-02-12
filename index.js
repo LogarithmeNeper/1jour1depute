6b7d9251635b88251de2c17d3b79077c8f517e11
Corentin Forler <cogk42@users.noreply.github.com>
2021-02-12 14:36:24 +0100

    logging

--- index.js
+++ index.js
@@ -13,14 +13,23 @@ const { API } = require('./api');
 
 async function main() {
   const id = await randomChoice();
+  console.log('<main>', 'id:', id);
   const depute = await getDeputeAsObject(id);
+  console.log('<main>', 'depute:', depute);
   // const imageData = (await got(depute.imageUrl)).body; // TODO: check exists
 
   const tweetTexts = deputeToTweets(depute);
+  console.log('<main>', 'tweetTexts:', tweetTexts);
   const tweets = tweetTexts.map(text => ({ text }));
+  console.log('<main>', 'tweets:', tweets);
 
   const api = API();
+  console.log('<main>', 'api:', api);
+
+  console.log('<main>', 'tweeting…');
   await api.tweetThread(tweets);
+
+  console.log('<main>', 'done.');
 }
 
 main().catch(err => {
