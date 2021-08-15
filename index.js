4dc4a9ab9c527366f69c1a4beb17dfb542e37f1c
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2021-08-15 18:16:57 +0200

    hotfix timezone

--- index.js
+++ index.js
@@ -9,28 +9,13 @@ const { getDeputeAsObject } = require('./fetch-informations-depute');
 const { deputeToTweets } = require('./stringify-depute');
 const { API } = require('./api');
 
-const now = changeTimezone(new Date(), 'Europe/Paris');
-const time = now.getHours().toString().padStart(2,'0') + ':' + now.getMinutes().toString().padStart(2,'0');
-const shouldRun = time > '10:57' && time < '11:03';
+const now = new Date();
+const target = new Date();
+target.setUTCHours(10, 0, 0);
+
+const dt = ((+now) - (+target)) / 1000;
+const shouldRun = Math.abs(dt) <= (5 * 60);
 const dryRun = (process.argv[2] === 'dry-run');
-
-if (shouldRun || dryRun) {
-  if (dryRun) {
-    console.log(`shouldRun = ${shouldRun}`);
-  }
-  main(dryRun).catch(err => {
-    console.error('ERROR');
-    console.error(err);
-    process.exit(12);
-  });
-}
-
-// https://stackoverflow.com/a/53652131
-function changeTimezone(date, ianatz) {
-  const invdate = new Date(date.toLocaleString('en-US', { timeZone: ianatz }));
-  const diff = date.getTime() - invdate.getTime();
-  return new Date(date.getTime() - diff);
-}
 
 function isDeputeValid(depute) {
   return (depute.mandats.length > 0);
@@ -75,3 +60,40 @@ async function main(dryRun = false, nRetries = 3) {
 
   console.log('done.');
 }
+
+if (shouldRun || dryRun) {
+  main(dryRun).catch(err => {
+    console.error('ERROR');
+    console.error(err);
+    process.exit(12);
+  });
+}
+
+/* 
+Fix on branch and try beforehand :
+
+const now = changeTimezone(new Date(), 'Europe/Paris');
+const time = now.getHours().toString().padStart(2,'0') + ':' + now.getMinutes().toString().padStart(2,'0');
+const shouldRun = time > '10:57' && time < '11:03';
+const dryRun = (process.argv[2] === 'dry-run');
+
+if (shouldRun || dryRun) {
+  if (dryRun) {
+    console.log(`shouldRun = ${shouldRun}`);
+  }
+  main(dryRun).catch(err => {
+    console.error('ERROR');
+    console.error(err);
+    process.exit(12);
+  });
+}
+
+// https://stackoverflow.com/a/53652131
+function changeTimezone(date, ianatz) {
+  const invdate = new Date(date.toLocaleString('en-US', { timeZone: ianatz }));
+  const diff = date.getTime() - invdate.getTime();
+  return new Date(date.getTime() - diff);
+}
+
+and delete lines 64-70
+*/
